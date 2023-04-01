50bc72555a58e789ef9c16df73f396ae1fc39172
Alexandr <makeitbund@gmail.com>
2023-04-01 17:26:57 +0300

    shop

--- src/hooks/useAdmin.ts
+++ src/hooks/useAdmin.ts
@@ -3,11 +3,14 @@ import { useMemo } from "react"
 export const useAdmin = () => {
     
     const getAdmin = useMemo(() => {
-        if (localStorage.getItem('admin')) {
-            return JSON.parse(localStorage.getItem('admin')!)
+        if (!localStorage.getItem('admin')) {
+            localStorage.setItem('admin', JSON.stringify(false))
+            return false
+        } else if (localStorage.getItem('admin') === 'true') {
+            return true
         } else {
             localStorage.setItem('admin', JSON.stringify(false))
-            return []
+            return false
         }
     }, [])
 
